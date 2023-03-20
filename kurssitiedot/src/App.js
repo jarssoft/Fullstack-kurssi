6d11491e6963f42c7c6feef8bcbae951d4f3e980
Jari Saari <jari.m.saari@gmail.com>
2023-03-20 23:50:04 +0200

    1.1 valmis

--- kurssitiedot/src/App.js
+++ kurssitiedot/src/App.js
@@ -6,30 +6,44 @@ const Header = (props) => {
   )//
 }
 
+const Content = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  )
+}
+
+const Total = (props) => {
+  let sum=0;
+  for(let i=0;i<props.part.length;i++){
+    sum=sum+props.part[i].exercises
+  }
+  return (
+    <p>Number of exercises {sum}</p>
+  )
+}
+
 const App = () => {
+
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+
+  const parts = [
+    { name: 'Fundamentals of React',    exercises: 10 },
+    { name: 'Using props to pass data', exercises:  7 },
+    { name: 'State of a component',     exercises: 14 },
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Content part={parts[0]} />
+      <Content part={parts[1]} />
+      <Content part={parts[2]} />
+      <Total part={parts} />
     </div>
   )
+
 }
 
 export default App
