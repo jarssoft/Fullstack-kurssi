f859e8da6821a35e46457a4684ffa7054a399e46
Jari Saari <jari.m.saari@gmail.com>
2023-03-23 16:41:42 +0200

    1.8 Valmis

--- osa 1/unicafe/src/App.js
+++ osa 1/unicafe/src/App.js
@@ -1,36 +1,46 @@
 import { useState } from 'react'
 
+const Statistics = ({good, neutral, bad}) => {
+
+    const all=good+neutral+bad;
+
+    return(
+      <>
+        <h1>statistic</h1>
+        <ul>
+          <li>good {good}</li>
+          <li>neutral {neutral}</li>
+          <li>bad {bad}</li>
+          <li>all {all}</li>
+          <li>average {(good-bad)/all}</li>
+          <li>positive {good/all*100} %</li>
+        </ul>
+      </>
+    )
+}//
+
 const App = () => {
-  // tallenna napit omaan tilaansa
+
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const all=good+neutral+bad;
-
+  
   return (
     <>
-    <h1>gife feedback</h1>
+    <h1>give feedback</h1>
     <div>
-      <button onClick={()=> setGood(good+1)}>
+      <button onClick={() => setGood(good+1)}>
         Good
       </button>
-      <button onClick={()=> setNeutral(neutral+1)}>
+      <button onClick={() => setNeutral(neutral+1)}>
         Neutral
       </button>
-      <button onClick={()=> setBad(bad+1)}>
+      <button onClick={() => setBad(bad+1)}>
         Bad
       </button>
     </div>
-    <h1>statistic</h1>
-      <ul>
-        <li>good {good}</li>
-        <li>neutral {neutral}</li>
-        <li>bad {bad}</li>
-        <li>all {all}</li>
-        <li>average {(good-bad)/all}</li>
-        <li>positive {good/all*100} %</li>
-      </ul>
+
+    <Statistics good={good} neutral={neutral} bad={bad} />
     </>
   )
 }
