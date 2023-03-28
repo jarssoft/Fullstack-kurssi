464681c0659a4839c7ce3e4cc8dc35daf0c19e1e
Jari Saari <jari.m.saari@gmail.com>
2023-03-28 14:31:31 +0300

    Tehtävä 2.4.

--- osa 2/kurssitiedot/src/App.js
+++ osa 2/kurssitiedot/src/App.js
@@ -21,11 +21,9 @@ const Content = ({course}) => {
 }
 
 const Total = (props) => {
-  const sum = 
-  props.course.parts.reduce((s, p) => {
-    console.log(s)
-    return (s + p.exercises)
-  }, 0 )
+  
+  const sum = props.course.parts.reduce((s, p) =>
+     (s + p.exercises), 0 )
 
   return (
     <p><b>Number of exercises {sum}</b></p>
@@ -43,37 +41,57 @@ const Course = ({course}) => {
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
 
-  return (
-    <div>
-      <Course course={course} />
-    </div>
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  return (courses.map(c =>
+      <div>
+        <Course course = {c} key = {c.id} />
+      </div>
+    )
   )
 }
 
