08ab2cc0faebaa54e5c0c75f7615651b23634e9b
Jari Saari <jari.m.saari@gmail.com>
2023-04-20 13:27:49 +0300

    Return phonebook.

--- osa 3/index.js
+++ osa 3/index.js
@@ -1,1 +1,33 @@
-console.log('hello world')
+const http = require('http')
+
+let notes = [
+    {
+      "name": "Arto Hellas",
+      "number": "040-123456",
+      "id": 1
+    },
+    {
+      "name": "Ada Lovelace",
+      "number": "39-44-5323523",
+      "id": 2
+    },
+    {
+      "name": "Dan Abramov",
+      "number": "12-43-234345",
+      "id": 3
+    },
+    {
+      "name": "Mary Poppendick",
+      "number": "39-23-6423122",
+      "id": 4
+    }
+]
+
+const app = http.createServer((request, response) => {
+      response.writeHead(200, { 'Content-Type': 'application/json' })
+      response.end(JSON.stringify(notes))
+    })
+    
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
