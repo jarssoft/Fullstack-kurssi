1a1f930573ac78f7b0a20f2901ebefada974aba0
Jari Saari <jari.m.saari@gmail.com>
2023-04-21 00:05:24 +0300

    Test POST

--- osa 3/index.js
+++ osa 3/index.js
@@ -42,6 +42,40 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
   })
 
+app.use(express.json())
+
+const generateId = () => {
+    const maxId = notes.length > 0
+        ? Math.max(...notes.map(n => n.id))
+        : 0
+    return maxId + 1
+}
+
+app.post('/api/persons', (request, response) => {
+
+    const body = request.body
+    console.log(body)  
+    response.json(body)
+
+    /*
+    if (!body.content) {
+        return response.status(400).json({ 
+        error: 'content missing' 
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        id: generateId(),
+    }
+
+    notes = notes.concat(note)
+
+    response.json(note)
+    */
+})
+
 app.get('/info', (req, res) => {
 
   const timeElapsed = Date.now();
