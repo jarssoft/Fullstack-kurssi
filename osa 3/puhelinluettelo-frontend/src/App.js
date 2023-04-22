7ba82a632346a8b5c3d919ed809c766e970f390a
Jari Saari <jari.m.saari@gmail.com>
2023-04-22 13:07:35 +0300

    Copy frontend here.

---
+++ osa 3/puhelinluettelo-frontend/src/App.js
@@ -0,0 +1,177 @@
+import personService from './services/persons'
+import { useState, useEffect } from 'react'
+
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  const footerStyle = {
+    color: message.color,
+    fontStyle: 'italic',
+    fontSize: 16,
+    margin: '10px'    
+  }
+
+  return (
+    <div style={footerStyle}>
+      {message.text}
+    </div>
+  )
+}
+
+const Person = (props) => {  
+  return (
+    <li>
+      <button onClick={props.onRemove}>poista</button>
+      {props.person.name} {props.person.number} 
+    </li>)
+}
+
+const Input = ({value, onChange, text}) => { 
+  return ( 
+    <div>
+        {text} :  
+        <input 
+          type="text"
+          value={value} 
+          onChange={onChange} 
+        />
+    </div>
+  )
+}
+
+const App = () => {
+
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [findText, setFindText] = useState('')
+  const [message, setMessage] = useState(null)
+  
+  useEffect(() => {
+    personService
+      .getAll()      
+      .then(response => {        
+        setPersons(response.data)
+      })
+  }, [])
+
+  const showMessage = (text, color) => {
+    const message = {
+      text,
+      color      
+    }
+    setMessage(message)
+    setTimeout(() => {
+      setMessage(null)        
+    }, 5000)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+
+    let newPerson = {
+      name: newName, 
+      number: newNumber 
+    }
+
+    let sama = persons.find((p)=>(p.name===newName));
+
+    if(sama===undefined){      
+      personService
+        .create(newPerson)
+        .then(returnedNote => {        
+          console.log(returnedNote);
+          setPersons(persons.concat(returnedNote))
+          setNewName('')
+          setNewNumber('')
+          showMessage(`Lisättiin ${newPerson.name}`, 'green')
+      })
+    }else{
+      if(window.confirm(`Korvataanko ${newName}?`)){
+        personService
+          .update(sama.id, newPerson)
+          .then(response => {
+              newPerson.id=sama.id            
+              setPersons(persons.map(p => (p.id != sama.id) ? p : newPerson));
+              setNewName('')
+              setNewNumber('')   
+              showMessage(`Korvattiin ${newPerson.name}`, 'green')
+        })
+      }
+    }
+  }
+
+  const removeName = (person) => {
+    if( window.confirm(`Poistetaanko ${person.name}?`)){
+      personService
+        .remove(person.id)
+        .then(response => {        
+          console.log(`${person} removed`);
+          setPersons(persons.filter(p => (p.id !=  person.id)))
+          showMessage(`Poistettiin ${person.name}`, 'green')
+          })
+        .catch(reason => {
+          showMessage(`Poisto ei onnistunut`, 'red')
+        })
+    }
+  }
+
+  const changeHandler = (hander) => ((event) => hander(event.target.value))
+  const removeHandler = (person) => (() => removeName(person))
+
+  return (
+    <div>
+
+      <h2>Puhelinluettelo</h2>
+
+      <form>
+        <Input 
+            text={"Etsi nimeä"} 
+            value={findText} 
+            onChange={changeHandler(setFindText)} 
+        />
+      </form>
+
+      <h2>Lisäys</h2>
+
+      <Notification message={message} />
+
+      <form onSubmit={addName}>
+        <Input 
+            text={"Nimi"} 
+            value={newName} 
+            onChange={changeHandler(setNewName)} 
+        />
+        <Input 
+            text={"Numero"} 
+            value={newNumber} 
+            onChange={changeHandler(setNewNumber)} 
+        />  
+        <div>
+          <button type="submit">Lisää</button>
+        </div>
+      </form>
+
+      <h2>Numerot</h2>
+      
+      <ul>
+      {
+        findText == ''
+          ? persons
+              .map(person => 
+              <Person person={person} key={person.name} onRemove={removeHandler(person)} />) 
+          : persons
+              .filter(person => (person.name.indexOf(findText) >= 0))
+              .map(person => 
+              <Person person={person} key={person.name} onRemove={removeHandler(person)} />) 
+      }
+      </ul>
+
+    </div>
+  )
+
+}
+
+export default App
---
+++ osa 3/puhelinluettelo-frontend/src/services/persons.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/persons'
+
+const getAll = () => {
+  return axios.get(baseUrl)
+}
+
+const create = newObject => {
+  //return axios.post(baseUrl, newObject)
+  const request = axios.post(baseUrl, newObject)
+  return request.then(response => response.data)
+}
+
+const update = (id, newObject) => {
+  return axios.put(`${baseUrl}/${id}`, newObject)
+}
+
+const remove = (id) => {
+  return axios.delete(`${baseUrl}/${id}`)
+}
+
+export default { 
+  getAll, 
+  create,
+  remove,
+  update 
+}
