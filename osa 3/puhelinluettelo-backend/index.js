1d198dac84f7c135bcec7b18822fbcc94fbcbb2a
Jari Saari <jari.m.saari@gmail.com>
2023-04-22 16:33:57 +0300

    Haetaan portti ympäristömuuttujasta.

--- osa 3/puhelinluettelo-backend/index.js
+++ osa 3/puhelinluettelo-backend/index.js
@@ -111,7 +111,14 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+/*
 const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+*/
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
