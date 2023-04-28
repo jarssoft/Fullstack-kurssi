01808663244bc11c5b4a9f39fbe53f92e639677d
Jari Saari <jari.m.saari@gmail.com>
2023-04-28 23:06:51 +0300

    3.20* Numeron validaatio.

--- osa 3/puhelinluettelo-backend/models/person.js
+++ osa 3/puhelinluettelo-backend/models/person.js
@@ -16,7 +16,17 @@ mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
   name: {type: String, minlength: 3, required: true},
-  number: String,
+  number:{
+    type: String,
+    minlength: 8,
+    validate: {
+      validator: function(v) {
+        return /\d{2,3}-\d{4}/.test(v);
+      },
+      message: props => `${props.value} ei ole puhelinnumero!`
+    },
+    required: [true, 'Puhelinumero puuttuu!']
+  }
 })
 
 personSchema.set('toJSON', {
