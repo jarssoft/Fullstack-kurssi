399dddb81fad443e7f0ae924248cf2232743562d
Jari Saari <jari.m.saari@gmail.com>
2023-05-18 12:35:28 +0300

    4.11*: blogilistan testit, step4

--- osa 4/blogilista/models/blog.js
+++ osa 4/blogilista/models/blog.js
@@ -4,7 +4,10 @@ const blogSchema = mongoose.Schema({
     title: String,
     author: String,
     url: String,
-    likes: Number
+    likes:  {
+      type: Number,
+      default: 0
+    }
   }).set('toJSON', {
     transform: (document, returnedObject) => {
       returnedObject.id = returnedObject._id.toString()
