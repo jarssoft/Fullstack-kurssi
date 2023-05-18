b14828d18521d516f6a81b5502e2acbf27e8a515
Jari Saari <jari.m.saari@gmail.com>
2023-05-18 10:13:12 +0300

    4.8: blogilistan testit, step 1

--- osa 4/blogilista/tests/blogs_api.test.js
+++ osa 4/blogilista/tests/blogs_api.test.js
@@ -1,14 +1,34 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
+
+const Blog = require('../models/blog')
+const initialBlogs = [
+  {
+    title: 'Pallopanoraamablogi',
+    author: "Janne",
+    url: 'pallopanoraamablogi.blogspot.com',
+    likes: 1,
+  },
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let noteObject = new Blog(initialBlogs[0])
+  await noteObject.save()
+})
 
 test('notes are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('there are one blog', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(1)
 })
 
 afterAll(async () => {
