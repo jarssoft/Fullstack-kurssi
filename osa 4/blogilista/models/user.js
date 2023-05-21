db300cb9ab13299573a9a6260497b6435d199f8f
Jari Saari <jari.m.saari@gmail.com>
2023-05-22 00:07:34 +0300

    Piilota passwordHash listauksesta.

--- osa 4/blogilista/models/user.js
+++ osa 4/blogilista/models/user.js
@@ -18,6 +18,7 @@ const userSchema = mongoose.Schema({
       returnedObject.id = returnedObject._id.toString()
       delete returnedObject._id
       delete returnedObject.__v
+      delete returnedObject.passwordHash
     }
   })
 
