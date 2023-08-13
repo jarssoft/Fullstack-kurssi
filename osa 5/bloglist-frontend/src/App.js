0c9e1d2b7b02ab670257d7441f117572efadb35c
Jari Saari <jari.m.saari@gmail.com>
2023-08-13 23:46:58 +0300

    Lisää kirjautumislomake.

--- osa 5/bloglist-frontend/src/App.js
+++ osa 5/bloglist-frontend/src/App.js
@@ -4,12 +4,44 @@ import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('') 
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
   }, [])
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <form>
+          <div>
+            username
+              <input
+              type="text"
+              value={username}
+              name="Username"
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            password
+              <input
+              type="password"
+              value={password}
+              name="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type="submit">login</button>
+        </form>
+      </div>
+    )
+  }
 
   return (
     <div>
