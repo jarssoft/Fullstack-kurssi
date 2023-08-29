4ec96ec6510f3e3bc5a4413462b562275f7f498c
Jari Saari <jari.m.saari@gmail.com>
2023-08-29 14:40:49 +0300

    Määrittele Toggablelle PropType

--- osa 5/bloglist-frontend/src/components/Toggable.js
+++ osa 5/bloglist-frontend/src/components/Toggable.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 import { useState, useImperativeHandle, forwardRef } from 'react'
 
 const Togglable = forwardRef((props, ref) => {
@@ -27,5 +28,9 @@ const Togglable = forwardRef((props, ref) => {
     </div>
   )
 })
+
+Togglable.propTypes = {
+  buttonLabel: PropTypes.string.isRequired
+}
 
 export default Togglable
