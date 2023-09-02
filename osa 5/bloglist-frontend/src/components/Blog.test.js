83f8e2ec1fa4abfcd025b51e1dd0d7e18e90bb3a
Jari Saari <jari.m.saari@gmail.com>
2023-09-02 12:02:58 +0300

    Varmista toggleview-kutsu.

--- osa 5/bloglist-frontend/src/components/Blog.test.js
+++ osa 5/bloglist-frontend/src/components/Blog.test.js
@@ -1,18 +1,35 @@
 import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
+const blog = {
+  title: 'BlogTitle',
+  author: 'BlogAuthor',
+  url: 'BlogURL',
+  likes: 5
+}
+
 test('renders content', () => {
-  const blog = {
-    title: 'BlogTitle',
-    author: 'BlogAuthor',
-    url: 'BlogURL',
-    likes: 5
-  }
  
   render(<Blog blog={blog} />)
 
   const element = screen.getByText('BlogTitle')
   expect(element).toBeDefined()
+})
+
+test('lisätiedot näytetään, kun nappia on painettu ', async () => {
+
+  const mockHandler = jest.fn()
+
+  render(
+    <Blog blog={blog} toggleview={mockHandler} />
+  )
+
+  const user = userEvent.setup()
+  const button = screen.getByText('Näytä')
+  await user.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(1)
 })
