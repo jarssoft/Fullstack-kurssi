7dac96dad8cf1371711ed92e37bd1612602f59be
Jari Saari <jari.m.saari@gmail.com>
2023-09-05 17:32:17 +0300

    Tehtävä 5.17

--- osa 5/bloglist-frontend/cypress/e2e/spec.cy.js
+++ osa 5/bloglist-frontend/cypress/e2e/spec.cy.js
@@ -1,7 +1,13 @@
 describe('Blog app', () => {
-  it('front page can be opened', () => {
+
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/tests/reset')
     cy.visit('http://localhost:3000')
+  })
+
+  it('Login form is shown', () => {
     cy.contains('Log in to application')
     //cy.contains('Note app, Department of Computer Science, University of Helsinki 2023')
   })
+
 })
