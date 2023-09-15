813540808cf3843534aa7e05cb818b5a1f1a58a0
Jari Saari <jari.m.saari@gmail.com>
2023-09-15 23:34:22 +0300

    Testit menevät läpi (6.1)

--- osa 6/unicafe-redux/src/reducer.test.js
+++ osa 6/unicafe-redux/src/reducer.test.js
@@ -14,6 +14,7 @@ describe('unicafe reducer', () => {
       type: 'DO_NOTHING'
     }
 
+    deepFreeze(state)
     const newState = counterReducer(undefined, action)
     expect(newState).toEqual(initialState)
   })
@@ -32,4 +33,74 @@ describe('unicafe reducer', () => {
       bad: 0
     })
   })
+
+  test('good is incremented multiple times', () => {
+    const action = {
+      type: 'GOOD'
+    }
+    let state = initialState
+
+    deepFreeze(state)
+    
+    for (let i = 1; i <= 10; i++) {
+      state = counterReducer(state, action)
+    }
+
+    expect(state).toEqual({
+      good: 10,
+      ok: 0,
+      bad: 0
+    })
+  })
+
+  test('neutral is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('reset clears all', () => {
+    const action1 = {
+      type: 'GOOD'
+    }
+    const action2 = {
+      type: 'ZERO'
+    }
+
+    let state = initialState
+    deepFreeze(state)
+    state = counterReducer(state, action1)
+    const newState = counterReducer(state, action2)
+    
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 0
+    })
+  })
+
 })
