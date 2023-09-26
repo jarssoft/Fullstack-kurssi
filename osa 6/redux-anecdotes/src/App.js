9ad7da8b07f36103e3157edebf3b8de59fddd0ab
Jari Saari <jari.m.saari@gmail.com>
2023-09-26 21:58:02 +0300

    Hae anekdootit JSON Serverillä toteutetusta backendistä.

--- osa 6/redux-anecdotes/src/App.js
+++ osa 6/redux-anecdotes/src/App.js
@@ -2,8 +2,21 @@ import Notification from './components/Notification'
 import Filter from './components/Filter'
 import CreateNew from './components/Create'
 import List from './components/List'
+import { useEffect } from 'react'
+import anecdoteService from './services/anecdotes'
+import { setAnecdotes } from './reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  
+  useEffect(() => {
+    anecdoteService.getAll().then(notes => {
+      dispatch(setAnecdotes(notes))
+    })
+  }, [dispatch])
+
   return (
     <div>
       <Notification />
