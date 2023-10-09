0044e4bbad4945b6503c43f73da3af9f70dab6a0
Jari Saari <jari.m.saari@gmail.com>
2023-10-09 13:05:14 +0300

    anekdoottien lisääminen palvelimelle querylla

--- osa 6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ osa 6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,11 +1,25 @@
+import { useQuery, useMutation } from '@tanstack/react-query'
+import axios from 'axios'
+
 const AnecdoteForm = () => {
+
+  const createNote = newNote => {
+    console.log(`newNote ${newNote}`)
+    axios
+    .post('http://localhost:3001/anecdotes', newNote)
+    .then(res => res.data)
+  }
+  
+  const newNoteMutation = useMutation({ mutationFn: createNote })
+
 
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    console.log('new anecdote')
-}
+    newNoteMutation.mutate({ content, votes: 0 })
+    console.log(`new anecdote ${content}`)
+  }
 
   return (
     <div>
