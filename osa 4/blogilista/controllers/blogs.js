7e3420cdebe2603d3e715cd37b5be013254749f4
Jari Saari <jari.m.saari@gmail.com>
2023-11-21 13:55:29 +0200

    Lisättiin backendiin kommenttien lisääminen.

--- osa 4/blogilista/controllers/blogs.js
+++ osa 4/blogilista/controllers/blogs.js
@@ -1,77 +1,75 @@
-const jwt = require('jsonwebtoken')
-const blogsRouter = require('express').Router()
-const Blog = require('../models/blog')
-const User = require('../models/user')
+const jwt = require("jsonwebtoken");
+const blogsRouter = require("express").Router();
+const Blog = require("../models/blog");
+const User = require("../models/user");
 
-blogsRouter.get('/', async (request, response, next) => {
-   
-  try{
-      let blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
-      response.json(blogs)
-    } catch(exception) {
-      next(exception)
-    }
+blogsRouter.get("/", async (request, response, next) => {
+  try {
+    let blogs = await Blog.find({}).populate("user", {
+      username: 1,
+      name: 1,
+      id: 1,
+    });
+    response.json(blogs);
+  } catch (exception) {
+    next(exception);
+  }
+});
 
-  })
-  
-blogsRouter.post('/', async (request, response, next) => {
-  const blog = new Blog(request.body)
+blogsRouter.post("/", async (request, response, next) => {
+  const blog = new Blog(request.body);
 
   //console.log(`pyytäjä ${request.user}`);
 
-  try{
-     
+  try {
     if (!request.user) {
-      return response.status(401).json({ error: 'token invalid' })
+      return response.status(401).json({ error: "token invalid" });
     }
-    const user = await User.findById(request.user)
+    const user = await User.findById(request.user);
 
-    blog.user = user.toJSON().id
-    
-    const saved = await blog.save()
-    console.log(saved)
-    
-    user.blogs = user.blogs.concat(saved.id)
-    console.log(user)
-    
-    await user.save()
-    response.status(201).json(saved)
+    blog.user = user.toJSON().id;
 
-  } catch(exception) {
-    next(exception)
+    const saved = await blog.save();
+    console.log(saved);
+
+    user.blogs = user.blogs.concat(saved.id);
+    console.log(user);
+
+    await user.save();
+    response.status(201).json(saved);
+  } catch (exception) {
+    next(exception);
   }
+});
 
-})
-
-blogsRouter.delete('/:id', async (request, response, next) => {
-
+blogsRouter.delete("/:id", async (request, response, next) => {
   console.log("oma delete-metodi");
 
-  try{
-
+  try {
     if (!request.user) {
-      return response.status(401).json({ error: 'token invalid' })
+      return response.status(401).json({ error: "token invalid" });
     }
 
-    const blog = await Blog.findById(request.params.id)
+    const blog = await Blog.findById(request.params.id);
     console.log(`Poistetaan ${blog}:n blogi`);
 
-    const user = await User.findById(request.user)
+    const user = await User.findById(request.user);
     console.log(`Pyytäjä: ${user}`);
 
-    if ( blog.user.toString() !== request.user.toString() ){
-      return response.status(403).json({ error: 'user is not owner this data' })  
+    if (blog.user.toString() !== request.user.toString()) {
+      return response
+        .status(403)
+        .json({ error: "user is not owner this data" });
     }
 
-    await Blog.findByIdAndRemove(request.params.id)
-    response.status(204).end()
-  } catch(exception) {
-    next(exception)
+    await Blog.findByIdAndRemove(request.params.id);
+    response.status(204).end();
+  } catch (exception) {
+    next(exception);
   }
+});
 
-})
-
-blogsRouter.put('/:id', async (request, response, next) => {
+blogsRouter.put("/:id", async (request, response, next) => {
   const body = request.body;
 
   const person = {
@@ -79,15 +77,64 @@ blogsRouter.put('/:id', async (request, response, next) => {
     author: body.author,
     url: body.url,
     likes: body.likes,
+  };
+
+  try {
+    await Blog.findByIdAndUpdate(request.params.id, person, { new: true });
+    response.status(204).end();
+  } catch (exception) {
+    next(exception);
   }
-  
-  try{
-    await Blog.findByIdAndUpdate(request.params.id, person, {new: true})
-    response.status(204).end()
-  } catch(exception) {
-    next(exception)
+});
+
+blogsRouter.post("/:id/comments", async (request, response, next) => {
+  const body = request.body;
+
+  try {
+    console.log(request.params.id);
+    let blog = await Blog.findById(request.params.id);
+
+    console.log(blog);
+
+    comments = [body.comment];
+    if (blog.comments) {
+      comments = comments.concat(blog.comments);
+    }
+
+    const newblog = {
+      title: blog.title,
+      author: blog.author,
+      url: "blog.url",
+      likes: blog.likes,
+      comments: comments,
+    };
+
+    console.log(newblog);
+
+    try {
+      await Blog.findByIdAndUpdate(request.params.id, newblog, { new: true });
+      response.status(204).end();
+    } catch (exception) {
+      next(exception);
+    }
+  } catch (exception) {
+    next(exception);
   }
 
+  /*
+  const body = request.body;
+
+  const person = {
+    comments: [body.comment],
+  };
+
+  try {
+    await Blog.findByIdAndUpdate(request.params.id, person, { new: true });
+    response.status(204).end();
+  } catch (exception) {
+    next(exception);
+  }
+  */
 });
-  
-module.exports = blogsRouter
+
+module.exports = blogsRouter;
--- osa 4/blogilista/utils/middleware.js
+++ osa 4/blogilista/utils/middleware.js
@@ -1,59 +1,59 @@
-const logger = require('./logger')
-const jwt = require('jsonwebtoken')
+const logger = require("./logger");
+const jwt = require("jsonwebtoken");
 
 const requestLogger = (request, response, next) => {
-  logger.info('Method:', request.method)
-  logger.info('Path:  ', request.path)
-  logger.info('Body:  ', request.body)
-  logger.info('---')
-  next()
-}
+  logger.info("Method:", request.method);
+  logger.info("Path:  ", request.path);
+  logger.info("Body:  ", request.body);
+  logger.info("---");
+  next();
+};
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
 const errorHandler = (error, request, response, next) => {
-  
   console.log("oma virheenkäsittely");
-  logger.error(error.message)
+  logger.error(error.message);
 
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  } else if (error.name ===  'JsonWebTokenError') {
-    return response.status(400).json({ error: 'token missing or invalid' })
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  } else if (error.name === "JsonWebTokenError") {
+    return response.status(400).json({ error: "token missing or invalid" });
   }
 
-  next(error)
-}
+  next(error);
+};
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization') 
-  if (authorization && authorization.startsWith('Bearer ')) {
-        return authorization.replace('Bearer ', '')  
-  }  return null
-}
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "");
+  }
+  return null;
+};
 
 const tokenExtractor = (request, response, next) => {
   // tokenin ekstraktoiva koodi
-  request.token = getTokenFrom(request)
-  next()
-}
+  request.token = getTokenFrom(request);
+  next();
+};
 
 const userExtractor = (request, response, next) => {
-  if(request.token){
-    const decodedToken = jwt.verify(request.token, process.env.SECRET)
-    request.user = decodedToken.id  
+  if (request.token) {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET);
+    request.user = decodedToken.id;
   }
-  next()
-}
+  next();
+};
 
 module.exports = {
   requestLogger,
   unknownEndpoint,
   errorHandler,
   tokenExtractor,
-  userExtractor
-}
+  userExtractor,
+};
