d89f9a98667b7ba968fb2dd342e20bf8e3fce2eb
Jari Saari <jari.m.saari@gmail.com>
2024-02-27 23:14:20 +0200

    8.11 lomake

--- osa 8/library-frontend/src/components/Authors.js
+++ osa 8/library-frontend/src/components/Authors.js
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import { useQuery } from "@apollo/client";
 import { ALL_PERSONS } from "../queries";
 
 const Authors = (props) => {
+  const [name, setName] = useState("");
+  const [born, setBorn] = useState("");
   const result = useQuery(ALL_PERSONS);
 
   if (result.loading) {
@@ -11,6 +14,16 @@ const Authors = (props) => {
   if (!props.show) {
     return null;
   }
+
+  const submit = async (event) => {
+    event.preventDefault();
+
+    console.log("set birth...");
+    //createPerson({ variables: { title, author, published, genres } });
+
+    //setName("");
+    setBorn(0);
+  };
 
   const authors = result.data.allAuthors;
 
@@ -33,6 +46,25 @@ const Authors = (props) => {
           ))}
         </tbody>
       </table>
+      <h2>Set birth</h2>
+
+      <form onSubmit={submit}>
+        <div>
+          name
+          <input
+            value={name}
+            onChange={({ target }) => setName(target.value)}
+          />
+        </div>
+        <div>
+          born
+          <input
+            value={born}
+            onChange={({ target }) => setBorn(target.value)}
+          />
+        </div>
+        <button type="submit">set</button>
+      </form>
     </div>
   );
 };
