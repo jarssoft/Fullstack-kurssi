677c766617f89a543d4a6ba48667f437e4b80544
Jari Saari <jari.m.saari@gmail.com>
2024-04-10 22:06:43 +0300

    tsconfig

--- osa 9/bmiCalculator.ts
+++ osa 9/bmiCalculator.ts
@@ -21,17 +21,13 @@ const calculateBmi = (height: number, mass: number): string => {
   const bmi = mass / ((height / 100) * (height / 100));
   if (bmi < 18.5) {
     return "Underweight (Unhealthy)";
-  }
-  if (bmi >= 18.5 && bmi <= 22.9) {
+  } else if (bmi >= 18.5 && bmi <= 22.9) {
     return "Normal range (Healthy)";
-  }
-  if (bmi > 22.9 && bmi <= 24.9) {
+  } else if (bmi > 22.9 && bmi <= 24.9) {
     return "Overweight I (At risk)";
-  }
-  if (bmi > 24.9 && bmi <= 29.9) {
+  } else if (bmi > 24.9 && bmi <= 29.9) {
     return "Overweight II (Moderately obese)";
-  }
-  if (bmi > 29.9) {
+  } else {
     return "Overweight III (Severely obese)";
   }
 };
