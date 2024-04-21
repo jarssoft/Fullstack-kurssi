bd67c2a52c3eb702bcc9477cf36c989a14420665
Jari Saari <jari.m.saari@gmail.com>
2024-04-21 11:45:50 +0300

    Check HealthCheck-entry

--- osa 9/patientor-backend/utils-entry.ts
+++ osa 9/patientor-backend/utils-entry.ts
@@ -4,6 +4,10 @@ const isString = (text: unknown): text is string => {
   return typeof text === "string" || text instanceof String;
 };
 
+const isNumber = (text: unknown): text is number => {
+  return typeof text === "number" || text instanceof Number;
+};
+
 const parseDiagnosisCodes = (object: unknown): Array<Diagnosis["code"]> => {
   if (!object || typeof object !== "object" || !("diagnosisCodes" in object)) {
     // we will just trust the data to be in correct form
@@ -12,10 +16,6 @@ const parseDiagnosisCodes = (object: unknown): Array<Diagnosis["code"]> => {
 
   return object.diagnosisCodes as Array<Diagnosis["code"]>;
 };
-
-//const isHospital = (param: string): param is "Hospital" => {
-//  return ["Hospital"].includes(param);
-//};
 
 const toNewEntry = (object: unknown): NewEntry => {
   if (!object || typeof object !== "object") {
@@ -30,7 +30,6 @@ const toNewEntry = (object: unknown): NewEntry => {
       isString(object.type) &&
       "specialist" in object &&
       isString(object.specialist) &&
-      "diagnosisCodes" in object &&
       "description" in object &&
       isString(object.description)
     )
@@ -40,36 +39,63 @@ const toNewEntry = (object: unknown): NewEntry => {
     );
   }
 
-  if (object.type == "Hospital") {
-    if (!("discharge" in object)) {
-      throw new Error("Incorrect data: discharge is missing");
-    }
-    if (
-      !(
-        object.discharge &&
-        typeof object.discharge === "object" &&
-        "date" in object.discharge &&
-        isString(object.discharge.date) &&
-        "criteria" in object.discharge &&
-        isString(object.discharge.criteria)
-      )
-    ) {
-      throw new Error("Incorrect data: discharge fields are missing");
+  switch (object.type) {
+    case "Hospital": {
+      if (!("discharge" in object)) {
+        throw new Error("Incorrect data: discharge is missing");
+      }
+      if (
+        !(
+          object.discharge &&
+          typeof object.discharge === "object" &&
+          "date" in object.discharge &&
+          isString(object.discharge.date) &&
+          "criteria" in object.discharge &&
+          isString(object.discharge.criteria)
+        )
+      ) {
+        throw new Error("Incorrect data: discharge fields are missing");
+      }
+
+      return {
+        date: object.date,
+        type: object.type,
+        specialist: object.specialist,
+        diagnosisCodes:
+          "diagnosisCodes" in object
+            ? parseDiagnosisCodes(object.diagnosisCodes)
+            : [],
+        description: object.description,
+        discharge: {
+          date: object.discharge.date,
+          criteria: object.discharge.criteria,
+        },
+      };
     }
 
-    return {
-      date: object.date,
-      type: object.type,
-      specialist: object.specialist,
-      diagnosisCodes: parseDiagnosisCodes(object.diagnosisCodes),
-      description: object.description,
-      discharge: {
-        date: object.discharge.date,
-        criteria: object.discharge.criteria,
-      },
-    };
+    case "HealthCheck": {
+      if (!("healthCheckRating" in object)) {
+        throw new Error("Incorrect data: discharge is missing");
+      }
+      if (!isNumber(object.healthCheckRating)) {
+        throw new Error("Incorrect data: discharge fields are missing");
+      }
+
+      return {
+        date: object.date,
+        type: object.type,
+        specialist: object.specialist,
+        diagnosisCodes:
+          "diagnosisCodes" in object
+            ? parseDiagnosisCodes(object.diagnosisCodes)
+            : [],
+        description: object.description,
+        healthCheckRating: object.healthCheckRating,
+      };
+    }
   }
-  throw new Error("Incorrect data: no corret type field.");
+
+  throw new Error("Incorrect data: no corret type-field.");
 };
 
 export default toNewEntry;
